8d3fc7e5d1acb0d08ae653f6037908bb2c8b5164
snehal <snehal.webroottech@gmail.com>
2025-04-28 23:29:16 +0530

    testimonial dynamic

--- app/Livewire/Testimonial.php
+++ app/Livewire/Testimonial.php
@@ -2,12 +2,24 @@
 
 namespace App\Livewire;
 
+use App\Models\Testimonial as ModelsTestimonial;
 use Livewire\Component;
 
 class Testimonial extends Component
 {
+
+   public $getTestimonials;
+
+   public function mount(){
+
+    $this->getTestimonials = ModelsTestimonial::where('status', 1)->get();
+    // dd($this->getTestimonials)
+}
+
     public function render()
     {
         return view('livewire.testimonial');
     }
+
+
 }
--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -150,7 +150,8 @@
                 deliver results. From Digital marketing to SEO, content strategy, or social media marketing in
                 Delhi, you name it and we will deliver it.</p>
         </div>
-        <div class="w-full h-full flex gap-3 min-[2550px]:gap-6 flex-wrap items-center justify-center mt-10 max-[599px]:mt-0">
+        <div
+            class="w-full h-full flex gap-3 min-[2550px]:gap-6 flex-wrap items-center justify-center mt-10 max-[599px]:mt-0">
             <div
                 class="card-1 w-[32%] h-[70vh] flex flex-col justify-end max-[1025px]:w-[48%] max-[599px]:w-full rounded-xl relative p-6 min-[2550px]:p-10 max-[599px]:p-4 bg-[#327493]">
                 <img class="w-[40%] absolute right-[10%] top-0" src="./assets/images/Homepage/social1.png" alt="">
@@ -174,16 +175,14 @@
                         <div
                             class="w-full cursor-pointer rounded-xl shadow pr-2 pl-4 py-2 bg-[#F5F7FF] text-[#163D4F] hover:text-[#3D3600] hover:bg-yellow-300 transition-all flex justify-between items-center">
                             <h1 class="text-[2.5vh] font-semibold">Social Media Community Management</h1>
-                            <img class="w-7 h-7 cursor-pointer" src="./assets/images/Homepage/arrowMain.png"
-                                alt="">
+                            <img class="w-7 h-7 cursor-pointer" src="./assets/images/Homepage/arrowMain.png" alt="">
                         </div>
                     </a>
                     <a href="{{ route('influencer-marketing') }}">
                         <div
                             class="w-full cursor-pointer rounded-xl shadow pr-2 pl-4 py-2 bg-[#F5F7FF] text-[#163D4F] hover:text-[#3D3600] hover:bg-yellow-300 transition-all flex justify-between items-center">
                             <h1 class="text-[2.5vh] font-semibold">Influencer Marketing</h1>
-                            <img class="w-7 h-7 cursor-pointer" src="./assets/images/Homepage/arrowMain.png"
-                                alt="">
+                            <img class="w-7 h-7 cursor-pointer" src="./assets/images/Homepage/arrowMain.png" alt="">
                         </div>
                     </a>
                 </div>
@@ -197,24 +196,21 @@
                         <div
                             class="w-full cursor-pointer rounded-xl shadow pr-2 pl-4 py-2 bg-[#F5F7FF] text-[#163D4F] hover:text-[#3D3600] hover:bg-yellow-300 transition-all flex justify-between items-center">
                             <h1 class="text-[2.5vh] font-semibold">Digital Marketing Strategy</h1>
-                            <img class="w-7 h-7 cursor-pointer" src="./assets/images/Homepage/arrowMain.png"
-                                alt="">
+                            <img class="w-7 h-7 cursor-pointer" src="./assets/images/Homepage/arrowMain.png" alt="">
                         </div>
                     </a>
                     <a href="{{ route('search-engine-marketing') }}">
                         <div
                             class="w-full cursor-pointer rounded-xl shadow pr-2 pl-4 py-2 bg-[#F5F7FF] text-[#163D4F] hover:text-[#3D3600] hover:bg-yellow-300 transition-all flex justify-between items-center">
                             <h1 class="text-[2.5vh] font-semibold">SEM (Search Engine Marketing)</h1>
-                            <img class="w-7 h-7 cursor-pointer" src="./assets/images/Homepage/arrowMain.png"
-                                alt="">
+                            <img class="w-7 h-7 cursor-pointer" src="./assets/images/Homepage/arrowMain.png" alt="">
                         </div>
                     </a>
                     <a href="{{ route('digital-social-media-marketing') }}">
                         <div
                             class="w-full cursor-pointer rounded-xl shadow pr-2 pl-4 py-2 bg-[#F5F7FF] text-[#163D4F] hover:text-[#3D3600] hover:bg-yellow-300 transition-all flex justify-between items-center">
                             <h1 class="text-[2.5vh] font-semibold">SEO (Social Media Marketing)</h1>
-                            <img class="w-7 h-7 cursor-pointer" src="./assets/images/Homepage/arrowMain.png"
-                                alt="">
+                            <img class="w-7 h-7 cursor-pointer" src="./assets/images/Homepage/arrowMain.png" alt="">
                         </div>
                     </a>
                     <a href="{{ route('online-reputation-management') }}">
@@ -382,123 +378,7 @@
 
     <!-- Teastimonials Section Starts Here -->
     <section class="fade w-full pt-32 max-[599px]:pt-6 overflow-hidden">
-        <div class="relative w-full h-full">
-            <div class="absolute w-[28%] right-0 -top-[12.2vh] min-[2550px]:-top-[13.3vh]  max-[1025px]:hidden z-30">
-                <img class="w-full h-full object-cover" src="./assets/images/Globalimages/testimonialBg.png"
-                    alt="">
-            </div>
-            <div class="w-full p-4 flex items-center flex-col">
-                <div class="headings flex items-center justify-start">
-                    <span
-                        class="capitalize font-[500] text-[3.5vw] max-[599px]:text-[9.5vw] max-[1025px]:text-[5vw]">testim</span>
-                    <span
-                        class="rounded-md text-white text-[3.5vw] max-[1025px]:text-[5vw] max-[599px]:text-[9.5vw] font-[600] px-2 py-2 leading-none text-center bg-gradient-to-r from-[#FF6F1F] to-[#E08A00]">
-                        onilas
-                    </span>
-                </div>
-            </div>
-
-            <!-- Swiper Container -->
-            <div class="swiper mySwiper relative flex mt-3 items-center justify-center">
-                <div class="swiper-wrapper">
-                    <!-- Testimonial Slide 1 -->
-                    <div class="swiper-slide relative flex items-center justify-center py-10 max-[599px]:py-0 px-6">
-                        <!-- Main Box (no fixed height) -->
-                        <div style="background: linear-gradient(120.02deg, #4F92F1 -6.05%, #4F92F1 9.3%, #2E548B 79.32%);"
-                            class="relative bg-[#4F92F1] via-[#4F92F1] to-[#2E548B] h-full w-[65%] max-[1025px]:w-[80%] max-[599px]:w-full text-white flex flex-col justify-between rounded-2xl p-10 max-[599px]:p-6 shadow-2xl overflow-hidden">
-
-                            <!-- Inner Quote Icon -->
-                            <div class="absolute w-[10%] max-[599px]:w-[20%] top-5 left-4 text-white select-none">
-                                <img class="w-full h-full object-cover" src="./assets/images/Homepage/quote.svg"
-                                    alt="">
-                            </div>
-
-
-                            <!-- Content -->
-                            <div class="z-10 relative">
-                                <h4
-                                    class="text-xl min-[2550px]:text-[1.3vw] font-medium mb-5 max-[599px]:mb-3 leading-tight tracking-wide uppercase">
-                                    What they say <br>
-                                    about us?</h4>
-                                <p class="text-sm min-[2550px]:text-[1.1vw]" style="line-height: 30px;">
-                                    Ask agreed answer rather joy nature admire wisdom. Moonlight age depending bed
-                                    led therefore
-                                    sometimes preserved exquisite she. An fail up so shot leaf wise in. Minuter
-                                    highest his
-                                    arrived for put and. Hopes lived by rooms oh in no death house. Ask agreed
-                                    answer rather joy nature admire wisdom. Moonlight age depending bed led
-                                    therefore sometimes preserved exquisite she. An fail up so shot leaf wise in.
-                                    Minuter highest his arrived for put and. Hopes lived by rooms oh in no death
-                                    house.
-                                </p>
-                            </div>
-
-                            <!-- Footer -->
-                            <div
-                                class="flex flex-col md:flex-row md:items-center md:justify-between mt-10 max-[599px]:mb-3 z-10 relative">
-                                <div>
-                                    <h3 class="text-4xl font-bold">Veronica Gleason</h3>
-                                    <p class="text-sm opacity-80 min-[2550px]:text-[0.8vw]">Dynamic Functionality Designer</p>
-                                </div>
-                                <div class="flex mt-3 md:mt-0 space-x-1 text-3xl">
-                                    <span>★</span><span>★</span><span>★</span><span>★</span><span
-                                        class="opacity-40">★</span>
-                                </div>
-                            </div>
-                        </div>
-
-                    </div>
-
-                    <!-- Testimonial Slide 2 -->
-                    <div class="swiper-slide relative flex items-center justify-center py-10 max-[599px]:py-0 px-6">
-                        <!-- Main Box (no fixed height) -->
-                        <div style="background: linear-gradient(120.02deg, #4F92F1 -6.05%, #4F92F1 9.3%, #2E548B 79.32%);"
-                            class="relative bg-[#4F92F1] via-[#4F92F1] to-[#2E548B] h-full w-[65%] max-[1025px]:w-[80%] max-[599px]:w-full text-white flex flex-col justify-between rounded-2xl p-10 max-[599px]:p-6 shadow-2xl overflow-hidden">
-
-                            <!-- Inner Quote Icon -->
-                            <div class="absolute w-[10%] max-[599px]:w-[20%] top-5 left-4 text-white select-none">
-                                <img class="w-full h-full object-cover" src="./assets/images/Homepage/quote.svg"
-                                    alt="">
-                            </div>
-
-
-                            <!-- Content -->
-                            <div class="z-10 relative">
-                                <h4
-                                    class="text-xl min-[2550px]:text-[1.3vw] font-medium mb-5 max-[599px]:mb-3 leading-tight tracking-wide uppercase">
-                                    What they say <br>
-                                    about us?</h4>
-                                <p class="text-sm min-[2550px]:text-[1.1vw]" style="line-height: 30px;">
-                                    Ask agreed answer rather joy nature admire wisdom. Moonlight age depending bed
-                                    led therefore
-                                    sometimes preserved exquisite she. An fail up so shot leaf wise in. Minuter
-                                    highest his
-                                    arrived for put and. Hopes lived by rooms oh in no death house. Ask agreed
-                                    answer rather joy nature admire wisdom. Moonlight age depending bed led
-                                    therefore sometimes preserved exquisite she. An fail up so shot leaf wise in.
-                                    Minuter highest his arrived for put and. Hopes lived by rooms oh in no death
-                                    house.
-                                </p>
-                            </div>
-
-                            <!-- Footer -->
-                            <div
-                                class="flex flex-col md:flex-row md:items-center md:justify-between mt-10 max-[599px]:mb-3 z-10 relative">
-                                <div>
-                                    <h3 class="text-4xl font-bold">Veronica Gleason</h3>
-                                    <p class="text-sm opacity-80 min-[2550px]:text-[0.8vw]">Dynamic Functionality Designer</p>
-                                </div>
-                                <div class="flex mt-3 md:mt-0 space-x-1 text-3xl">
-                                    <span>★</span><span>★</span><span>★</span><span>★</span><span
-                                        class="opacity-40">★</span>
-                                </div>
-                            </div>
-                        </div>
-
-                    </div>
-                </div>
-            </div>
-        </div>
+        @livewire('testimonial')
     </section>
     <!-- Teastimonials Section Ends Here -->
 
