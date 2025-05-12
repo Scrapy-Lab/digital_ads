61f2abe6a8e8bb8d9ef1f36992aed3a86c531181
snehal <snehal.webroottech@gmail.com>
2025-05-12 23:30:53 +0530

    image path updsated

--- app/Http/Controllers/MobileMarketingServiceController.php
+++ app/Http/Controllers/MobileMarketingServiceController.php
@@ -91,7 +91,7 @@ class MobileMarketingServiceController extends Controller
 
         $data = [
 
-            'banner_image' => "assets/images/Services/Mobile Marketing/Voice Sms/Bannner.png",
+            'banner_image' => "assets/images/Services/Mobile Marketing/Voice sms/banner.png",
             'section_1' => [
                 'banner_img' => 'assets/images/Services/Mobile Marketing/Voice Sms/img1.png',
                 'mobile_banner_img' => "assets/images/Services/Mobile Marketing/Voice Sms/img1.png",
@@ -171,7 +171,7 @@ class MobileMarketingServiceController extends Controller
 
         $data = [
 
-            'banner_image' => "assets/images/Services/Mobile Marketing/Bulk sms/Bannner.png",
+            'banner_image' => "assets/images/Services/Mobile Marketing/Bulk sms/banner.png",
             'section_1' => [
                 'banner_img' => 'assets/images/Services/Mobile Marketing/Bulk sms/img1.png',
                 'mobile_banner_img' => "assets/images/Services/Mobile Marketing/Bulk sms/img1.png",
