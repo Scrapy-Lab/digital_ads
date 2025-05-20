967cb420c3b00b30e549ab5f9af2472a1298411d
snehal <snehal.webroottech@gmail.com>
2025-05-20 22:06:13 +0530

    banner margin udpate

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -36,7 +36,7 @@
 
         @media (min-width: 1028px){
             section.fade.hero {
-                margin-top: 130px !important;
+                margin-top: 127px !important;
             }
         }
 
