53d50276f11bae19ef50639f03d90e5dda4a3895
snehal <snehal.webroottech@gmail.com>
2025-05-20 22:45:10 +0530

    code commented

--- resources/views/pages/visual-designing-services.blade.php
+++ resources/views/pages/visual-designing-services.blade.php
@@ -7,10 +7,10 @@
     </section>
 
     {{-- OLD CLASSESS --}}
-    <section
+    {{-- <section
         class="hero w-full h-[100vh] max-[599px]:hidden max-[1025px]:h-[40vh] max-[599px]:h-[80vh] bg-contain max-[1025px]:bg-cover max-[1025px]:bg-center bg-no-repeat mt-20 max-[599px]:mt-10 max-[599px]:pt-20 max-[599px]:bg-[60%] flex items-center justify-start px-20 max-[1025px]:px-10 max-[599px]:px-4"
         style="background-image: url('{{ $data['banner_image'] }}');">
-    </section>
+    </section> --}}
 
     <!-- Mobile Hero Section -->
     <section
