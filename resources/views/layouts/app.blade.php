f2a7659e33f3dfb3f34975f4a1a1a856cc1ae220
snehal <snehal.webroottech@gmail.com>
2025-05-20 22:07:19 +0530

    css udpate

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -36,7 +36,7 @@
 
         @media (min-width: 1028px){
             section.fade.hero {
-                margin-top: 127px !important;
+                margin-top: 118px !important;
             }
         }
 
