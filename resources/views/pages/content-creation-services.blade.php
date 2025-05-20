2e180f35b76d05cbfecfd4c96cc45698bb1fd6d0
snehal <snehal.webroottech@gmail.com>
2025-05-20 21:04:42 +0530

    condtion added

--- resources/views/pages/content-creation-services.blade.php
+++ resources/views/pages/content-creation-services.blade.php
@@ -9,7 +9,9 @@
     <section
     class="fade hero w-screen h-[75vh] mt-20 bg-[50%] max-[599px]:hidden max-[1025px]:h-[40vh] max-[599px]:h-[80vh] bg-cover max-[1025px]:bg-center bg-no-repeat max-[599px]:bg-[60%] flex items-center justify-start px-20 max-[1025px]:px-10 max-[599px]:px-4"
     style="background-image: url('{{ $data['banner_image'] }}');">
-    <button style="margin-top: 200px;"
+    <button @if (\Request::route()->getName() === 'content-writing')
+                style="margin-top: 200px;"
+    @endif
         class="text-white min-[2550px]:text-[2vw] min-[2550px]:translate-x-[22%] min-[2550px]:translate-y-48 max-[599px]:text-[3vw] {{ \Request::route()->getName() == "social-media-marketing" ? 'translate-x-[80%]': 'translate-x-[40%]' }} max-[599px]:translate-x-[-0%] translate-y-24 max-[599px]:translate-y-[130%] font-bold px-16 py-3 max-[599px]:py-2 max-[599px]:px-10 border-2 border-white rounded-md hover:bg-white hover:text-[#003366] transition-all duration-300">
         Talk to us
     </button>
